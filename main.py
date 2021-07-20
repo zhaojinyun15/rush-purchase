4fbcc9dfecebc0a36e496e651562c0fc6ac32f2f
zjy <zhaojinyun15@outlook.com>
2021-07-20 22:58:27 +0800

    add thread control

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from jingdong import run_for_jingdong
-from taobao import run_for_taobao
+import datetime
 
+from webdriver_rush import JingdongRush
 
 if __name__ == '__main__':
     url = r'the goods page you want to purchase'
-    # run_for_taobao.run(url)
-    run_for_jingdong.run(url)
+    ref_time = datetime.datetime.strptime('2020-01-01 12:00:00', '%Y-%m-%d %H:%M:%S')
+    thread_num = 10
+
+    for i in range(thread_num):
+        ref_time = ref_time + datetime.timedelta(seconds=0.1)
+        jd = JingdongRush(url=url, ref_time=ref_time, thread_name=f'thread_{i}', log_level='DEBUG')
+        jd.start()
--- webdriver_rush.py
+++ webdriver_rush.py
@@ -1,6 +1,7 @@
 import abc
+import datetime
 import logging
-import sys
+import threading
 import time
 
 from selenium import webdriver
@@ -9,14 +10,17 @@ from selenium.common.exceptions import NoSuchElementException
 import log_factory
 
 
-class MyRush(metaclass=abc.ABCMeta):
-    def __init__(self, url, driver=None, log_level=None):
+class MyRush(threading.Thread, metaclass=abc.ABCMeta):
+    def __init__(self, url, ref_time, thread_name, driver=None, log_level=None):
+        super().__init__()
         self.url = url
+        self.ref_time = ref_time
+        self.setName(thread_name)
 
         # create a webdriver
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-automation'])
-        options.add_experimental_option('prefs', {"profile.managed_default_content_settings.images": 2})
+        # options.add_experimental_option('prefs', {"profile.managed_default_content_settings.images": 2})
         if driver is None:
             self.wd = webdriver.Chrome(options=options)
         else:
@@ -26,8 +30,6 @@ class MyRush(metaclass=abc.ABCMeta):
         self._log_init(log_level)
 
     def _log_init(self, log_level):
-        # logging.basicConfig(format='%(asctime)s - %(name)s - {%(threadName)s} [%(levelname)s] : %(message)s',
-        #                     stream=sys.stdout)
         self.logger = logging.getLogger(str(self))
         self.logger.setLevel(logging.INFO if log_level is None else log_level)
         self.logger.addHandler(log_factory.h1)
@@ -46,7 +48,7 @@ class MyRush(metaclass=abc.ABCMeta):
 
     def login(self):
         self.logger.info('Please login in 60 seconds!')
-        time.sleep(60)
+        time.sleep(120)
 
     @abc.abstractmethod
     def run(self):
@@ -83,32 +85,51 @@ class JingdongRush(MyRush):
     def run(self):
         self.wd.get(self.url)
         self.login()
+        self._check_ref_time(self.ref_time)
+        self.wd.get(self.url)
         while True:
-            while True:
-                try:
-                    self.wd.find_element_by_link_text("加入购物车").click()
-                    print('"加入购物车" success')
-                    break
-                except NoSuchElementException:
-                    self.logger.debug('"加入购物车" failed')
-            while True:
-                try:
-                    self.wd.find_element_by_link_text("去购物车结算").click()
-                    print('"去购物车结算" success')
-                    break
-                except NoSuchElementException:
-                    self.logger.debug('"去购物车结算" failed')
-            while True:
-                try:
-                    self.wd.find_element_by_link_text("去结算").click()
-                    print('"去结算" success')
-                    break
-                except NoSuchElementException:
-                    self.logger.debug('"去结算" failed')
+            try:
+                self.wd.find_element_by_link_text("加入购物车").click()
+                self.logger.info('"加入购物车" success')
+                break
+            except NoSuchElementException:
+                self.logger.debug('"加入购物车" failed')
+        while True:
+            try:
+                self.wd.find_element_by_link_text("去购物车结算").click()
+                self.logger.info('"去购物车结算" success')
+                break
+            except NoSuchElementException:
+                self.logger.debug('"去购物车结算" failed')
+        while True:
+            try:
+                self.wd.find_element_by_link_text("去结算").click()
+                self.logger.info('"去结算" success')
+                break
+            except NoSuchElementException:
+                self.logger.debug('"去结算" failed')
+        while True:
+            try:
+                self.wd.find_element_by_id('order-submit').click()
+                self.logger.info('"提交订单" success')
+                break
+            except NoSuchElementException:
+                self.logger.debug('"提交订单" failed')
+        while True:
+            pass
+
+    def _check_ref_time(self, ref_time):
+        self.logger.info(f'ref_time: {str(ref_time)}')
+        while True:
+            now = datetime.datetime.now()
+            if now >= ref_time:
+                break
+            self.logger.debug('now < ref_time')
+        self.logger.info('now time is greater than ref_time')
 
 
 if __name__ == '__main__':
     # tb = TaobaoRush('https://www.taobao.com')
     # tb.url_test()
-    jd = JingdongRush('https://item.jd.com/100021367452.html')
+    jd = JingdongRush('https://www.jd.com/', '2021-07-20 21:31:00')
     jd.url_test()
